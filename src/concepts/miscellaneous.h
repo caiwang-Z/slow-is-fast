e8b7585824933507b216e595dddadbfc5295e042
Caiwang.Zuo <Caiwang.Zuo@chromasens.de>
2024-04-22 12:14:09 +0200

    type traits basic

---
+++ src/concepts/miscellaneous.h
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <algorithm>
+
+namespace TestNewFeatures {
+	void testStdClamp() {
+		const auto min = 42.1;
+		const auto max = 56.2;
+		const auto val1 = 31;
+		const auto val2 = 45;
+		const auto val3 = 99;
+		const auto res1 = std::clamp(static_cast<double>(val1), min, max); // 31
+		const auto res2 = std::clamp(static_cast<double>(val2), min, max); // 45
+		const auto res3 = std::clamp(static_cast<double>(val3), min, max); // 99
+	}
+
+}
+
+namespace TestTypeTraits {
+	template <typename T>
+	void printInfo(T value) {
+		if constexpr (std::is_integral<T>::value) {
+			std::cout << "integer: " << value << std::endl;
+		}
+		else if constexpr (std::is_floating_point<T>::value) {
+			std::cout << "double: " << value << std::endl;
+		}
+		else {
+			std::cout << "others:" << std::endl;
+		}
+	}
+
+	void testTypeTraitsBasic(){
+		printInfo(10);      // 10
+		printInfo(3.14);    // 3.14
+		printInfo("test");  // test
+	}
+
+	// std::true_type and std::false
+}
+
+void test() {
+
+}
