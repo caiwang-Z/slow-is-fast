ff7900181ce2a27313b9b882833c7e9db3679565
Caiwang.Zuo <caiwang.zuo@chromasens.de>
2024-05-13 00:12:05 +0200

    cmake template

---
+++ cmake/options.cmake
@@ -0,0 +1,4 @@
+option(CS_IMGPROC_EVALUATE "Build Evaluation Sample" ON)
+option(VSC_BUILD_CALIBRATION_GUI "Build calibration GUI using Qt" ON)
+option(PROJECT_HIDE_CONSOLE_WINDOW "Hide console window when running target training tool in Release configuration - ON by default" ON)
+
