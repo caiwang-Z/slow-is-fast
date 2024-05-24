4e8509687c15117aa01eca5dc9ab9bdba0813044
Caiwang.Zuo <caiwang.zuo@chromasens.de>
2024-05-24 21:52:22 +0200

    std::tuple

--- src/concepts/testStdContainer.h
+++ src/concepts/testStdContainer.h
@@ -364,6 +364,32 @@ namespace TestStdDequeUsage {
 	}
 }
 
+namespace TestStdTupleBaic {
+	std::tuple<double, char, std::string> get_student(int id)
+	{
+		switch (id)
+		{
+		case 0: return { 3.8, 'A', "Lisa Simpson" };
+		case 1: return { 2.9, 'C', "Milhouse Van Houten" };
+		case 2: return { 1.7, 'D', "Ralph Wiggum" };
+		case 3: return { 0.6, 'F', "Bart Simpson" };
+		}
+
+		throw std::invalid_argument("id");
+	}
+
+
+	void test() {
+		const auto student0 = get_student(0);
+		std::cout << "ID: 0, "
+			<< "GPA: " << std::get<0>(student0) << ", "
+			<< "grade: " << std::get<1>(student0) << ", "
+			<< "name: " << std::get<2>(student0) << '\n';
+
+	}
+
+}
+
 void test() {
 	//TestMyVector::test();
 	//TestMyVector::testAssignmentAndCopyConstructor();
