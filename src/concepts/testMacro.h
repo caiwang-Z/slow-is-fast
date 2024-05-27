c2c888d70e83aefbf1369171d4576436668b693a
Caiwang.Zuo <Caiwang.Zuo@chromasens.de>
2024-05-27 16:20:20 +0200

    macro->enumeration->data array

--- src/concepts/testMacro.h
+++ src/concepts/testMacro.h
@@ -1,5 +1,8 @@
 #pragma once
 #include <iostream>
+#include "utility.h"
+using UtilityNameSpace::myLog;
+using UtilityNameSpace::splitLine;
 
 namespace TestMacro {
 	// 1. defining func with macro
@@ -31,6 +34,99 @@ namespace TestCPPPredefinedMacros {
 		std::cout << "Current line: " << __LINE__ << std::endl;
 		std::cout << "Current func: " << __func__ << std::endl;
 	}
+
+}
+
+namespace TestOneMacroMultipleItems {
+
+#define GENERIC_ITEM \
+	STRING_ITEM, \
+	INT_ITEM, \
+	FLOAT_ITEM, \
+	ENUM_ITEM\
+
+
+	void testSwithCase() {
+
+		enum NewItems {
+			custom_item,
+			new_item,
+			GENERIC_ITEM
+
+		};
+
+		int i = 0;
+
+		NewItems newItem{NewItems::FLOAT_ITEM};
+
+		switch (newItem)
+		{
+		case STRING_ITEM:
+			i = 1;
+		case INT_ITEM:
+			i = 2;
+		case custom_item:
+			i = 3;
+		case new_item:
+			i = 4;
+		case FLOAT_ITEM:
+			i = 5;
+		case ENUM_ITEM:
+			i = 6;
+		default:
+			break;
+		}
+
+		myLog(i); // 5
+	
+	}
+
+	
+	void test() {
+		testSwithCase();
+	
+	}
+
+}
+
+namespace TestMacroCreateComplexDataStructure {
+/*
+In C or C++, macro definitions are made through a preprocessor, as opposed to namespace scopes. 
+Macros are global and, once defined, can be used throughout the entire compilation unit 
+(usually a source file and its contained header files). 
+Thus, if a macro is defined in namespace a in one file, it is accessible in namespace b in the same file.
+
+*/
+
+#define CUSTOM_GENERATED_INFOS \
+	{STRING_ITEM, 0X40000000, 80, false},\
+	{INT_ITEM, 0X50000000, 90, true},\
+	{FLOAT_ITEM, 0X60000000, 100, false},\
+	{ENUM_ITEM, 0X90000000, 199, true}\
+
+	enum CustomEnum
+	{
+		GENERIC_ITEM
+	}; // must transter macro to the enum struct
+
+	struct RegisterInfo {
+		unsigned id;
+		uint64_t address;
+		size_t size;
+		bool var_sized;
+
+	};
+
+	void test() {
+		RegisterInfo infoArrays[] = { CUSTOM_GENERATED_INFOS };
+		myLog(infoArrays[1].id); // 1
+		myLog(infoArrays[3].id); // 3
+		myLog(infoArrays[2].id); // 2
+	
+	}
+
+
+
 }
 
 
@@ -41,6 +137,10 @@ void test() {
 	int a = 2;
 #endif
 	TestCPPPredefinedMacros::test();
+	splitLine();
+	TestOneMacroMultipleItems::test();
+	splitLine();
+	TestMacroCreateComplexDataStructure::test();
 
 }
 
