f4596679c398f019a38dd8734b8ec7e19bbf16ee
Caiwang.Zuo <caiwang.zuo@chromasens.de>
2024-06-07 01:40:08 +0200

    std::execution in std::sort, std::for_each, std::transform

--- src/concepts/testStdFunctions.h
+++ src/concepts/testStdFunctions.h
@@ -8,9 +8,14 @@
 #include <numeric>
 #include <format>
 #include <execution>
+#include <random>
 
 using UtilityNameSpace::myLog;
 using UtilityNameSpace::splitLine;
+using UtilityNameSpace::Splitter;
+
+//#define PARALLEL
+
 
 namespace TestStaticAssert {
 
@@ -511,29 +516,43 @@ namespace TestStdIota {
 }
 
 namespace TestStdExecution {
+
 	// include <execution>
-	void testStdExecution() {
-		std::vector<int> vec = { 5, 2, 8, 1, 3 };
+#ifdef PARALLEL
+#include <execution>
+	namespace execution = std::execution;
+#else
+	enum class execution { seq, unseq, par_unseq, par };
+#endif
 
-		std::sort(std::execution::unseq, vec.begin(), vec.end());
+	void measure([[maybe_unused]] auto policy, std::vector<std::uint64_t> v)
+	{
+		const auto start = std::chrono::steady_clock::now();
+#ifdef PARALLEL
+		std::sort(policy, v.begin(), v.end());
+#else
+		std::sort(v.begin(), v.end());
+#endif
+		const auto finish = std::chrono::steady_clock::now();
+		std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(finish - start)
+			<< '\n';
+	};
+	void testExecution() {
+		SIFSplit;
+		std::vector<std::uint64_t> v(1'000'000);
+		std::mt19937 gen{ std::random_device{}() };
+		std::ranges::generate(v, gen);
 
-		for (int v : vec) {
-			std::cout << v << " ";
-		}
-		std::cout << std::endl;
+		measure(execution::seq, v);
+		measure(execution::unseq, v);
+		measure(execution::par_unseq, v);  // no parallel: 78ms, with parallel: 32ms
+		measure(execution::par, v);  // no parallel: 81ms, with parallel: 37ms
 	
 	}
-
-	void test() {
-		std::cout << "__func__: " << __func__ << std::endl;
-		testStdExecution();
-	
-	}
-
 }
 
 void test() {
-	TestStdExecution::test();
+	TestStdExecution::testExecution();
 	//TestStdIota::test();
 	//TestStdRefBasic::test();
 	//TestSTDBindBasic::test();
