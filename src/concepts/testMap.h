1b5f152325c5774772e4ff09de42ebfa47d4cf67
Caiwang.Zuo <caiwang.zuo@chromasens.de>
2024-07-21 10:32:24 +0200

    .clang-format and Ctrl k + Ctrl + d

--- src/concepts/testMap.h
+++ src/concepts/testMap.h
@@ -3,38 +3,30 @@
 #include <unordered_map>
 
 struct CityRecord {
-	std::string name;
-	uint64_t population;
-	double latitude, longitude;
-
+  std::string name;
+  uint64_t    population;
+  double      latitude, longitude;
 };
 
 namespace std {
-	template<>
-	struct hash<CityRecord> {  // make sure custom struct could be hashed
-		size_t operator()(const CityRecord& key) {
-			return hash<std::string>()(key.name);
-		}
-
-	};
-}
+template <>
+struct hash<CityRecord> {  // make sure custom struct could be hashed
+  size_t operator()(const CityRecord& key) { return hash<std::string>()(key.name); }
+};
+}  // namespace std
 
 namespace TestMapBasic {
-	// operator= access or insert specified element, Returns a reference to the value that is mapped to a key equivalent to key or x respectively, 
-	// performing an insertion if such key does not already exist.
-	// at: access specified element with bounds checking
-	using std::string;
+// operator= access or insert specified element, Returns a reference to the
+// value that is mapped to a key equivalent to key or x respectively, performing
+// an insertion if such key does not already exist. at: access specified element
+// with bounds checking
+using std::string;
 
+std::unordered_map<CityRecord, uint32_t> cityMap;
 
-
-	std::unordered_map<CityRecord, uint32_t> cityMap;
-
-
-	void test() {
-	}
-}
+void test() {}
+}  // namespace TestMapBasic
 
 void test() {
-	TestMapBasic::test();
-
+  TestMapBasic::test();
 }
